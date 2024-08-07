a5be1bc69c0718e3c6d7d3e36edc3fb254fbf94c
Sidney Ferracin Jr. <sidneyferracinjr@gmail.com>
2024-08-07 15:13:30 -0300

    feat: database connection

---
+++ src/main/java/com/app_rh/DataConfiguration.java
@@ -0,0 +1,39 @@
+package com.app_rh;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/apprh?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root"); 
+		return dataSource;
+		
+	}
+	
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+		adapter.setPrepareConnection(true);
+		
+		return adapter;
+	}
+}
+
+
+
---
+++ src/test/java/com/app_rh/AppRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
